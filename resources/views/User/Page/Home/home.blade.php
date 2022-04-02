85e03ce5e4744e3f67b925246cf99a98469129e1
rymarshandasw <rymarshandas@gmail.com>
2022-04-02 11:43:43 +0700

    Home belum beres

--- resources/views/User/Page/Home/home.blade.php
+++ resources/views/User/Page/Home/home.blade.php
@@ -2,26 +2,40 @@
 
 @section('content')
 
-    <div class="row mt-4">
-        <div class="col-lg 6 d-flex justify-content-center">
-            <div class="card" style="width:400px">
-                <img class="card-img-top" src="img_avatar1.png" alt="Card image">
-                <div class="card-body">
-                    <h4 class="card-title">John Doe</h4>
-                    <p class="card-text">Some example text.</p>
-                    <a href="#" class="btn btn-primary d-flex justify-content-center">See Profile</a>
-                </div>
+<div class="container py-3 bg-white" >
+    <img src="http://drive.google.com/uc?export=view&id=1YEMvaFTYusWrjThHEGISvpxN4oUZZ5cx" alt="foto" width="559px" height="70px" style="margin-left:auto; margin-right:auto; display:block; padding-top:24px ">
+        <div class="row mt-4 justify-content-center">
+          <div class="col-sm-6 d-flex justify-content-center" style="margin-bottom:20px;" >
+            <div class="card card-block" style="width: 300px; height:430px; border-radius: 25px; border-solid:1px; border-color:#D77409;">
+              <img src="http://drive.google.com/uc?export=view&id=12HjDeFfTn8KY7fKcinR4pxvHugeqG9hg" alt="foto" width="237px" height="235px" style="padding-left:80px; padding-top:48px">
+              <div class="card-body">
+                <p class="card-text" style="font-size:16px; font-family: Nunito; font-weight: 400; text-align: center;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis habitasse imperdiet sagittis, tincidunt aliquet adipiscing. Aliquet lectus iaculis neque morbi odio.</p>
+                <a class="btn btn-outline-light" style="background-color:#F58814;font-color:white;width:50%;border-radius:25px 25px 25px 25px; width: 182px; left: 336px; font-family: Nunito; margin-left: 40px" href="">ORDER LOAK.IN</a>
+              </div> 
             </div>
+          </div> 
+    
+          <div class="col-sm-6 d-flex justify-content-center" style="margin-bottom:20px;" >
+            <div class="card card-block" style="width: 300px; height:430px; border-radius: 25px; border-solid:1px; border-color:#D77409;">
+              <img src="http://drive.google.com/uc?export=view&id=1ccVV5FSxeuMvm9WxooFvxQqrOs1CTAGm" alt="foto" width="237px" height="235px" style="padding-left:80px; padding-top:48px">
+              <div class="card-body">
+                <p class="card-text" style="font-size:16px; font-family: Nunito; font-weight: 400; text-align: center;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis habitasse imperdiet sagittis, tincidunt aliquet adipiscing. Aliquet lectus iaculis neque morbi odio.</p>
+                <a class="btn btn-outline-light" style="background-color:#F58814;font-color:white;width:50%;border-radius:25px 25px 25px 25px; width: 182px; left: 336px; font-family: Nunito; margin-left: 40px" href="">SHOP NOW</a>
+              </div> 
+            </div>
+          </div> 
         </div>
-        <div class="col-lg-6 d-flex justify-content-center">
-            <div class="card" style="width:400px">
-                <img class="card-img-top" src="img_avatar1.png" alt="Card image">
-                <div class="card-body">
-                    <h4 class="card-title">John Doe</h4>
-                    <p class="card-text">Some example text.</p>
-                    <a href="#" class="btn btn-primary d-flex justify-content-center">See Profile</a>
-                </div>
-            </div>
+</div>
+    
+    <div class="container py-3 bg-white mt-3" >
+        <h3 style="font-family: 'Nunito'; font-style: normal; font-weight: 800; color:#D77409; text-align:center;">ABOUT US</h3>
+        <div class="row mt-3">
+          <div class="col-sm-5" style="font-family: 'Nunito'; font-style: normal; font-weight: 400; font-size: 18px; line-height: 29px; margin-top:50px">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu eget fusce varius enim vitae vitae vitae nunc enim. Vitae in ut morbi tellus mattis cras egestas et ultricies. Semper at sit enim semper cras magna vel id. Felis potenti ultricies sed ullamcorper viverra euismod amet.</p>
+          </div>
+          <div class="col-sm-5">
+              <img src="http://drive.google.com/uc?export=view&id=1n12_GxmVPOxNhzFLoIvty1NkP7ru8BAx" alt="Foto" width="408px" height="206px" style="margin-left:250px;">
+          </div>
         </div>
     </div>
 
