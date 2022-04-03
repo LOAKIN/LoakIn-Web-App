b474fa45b24069b91d149e3da1e6f28ca7c184c6
rymarshandasw <rymarshandas@gmail.com>
2022-04-03 21:30:07 +0700

    testi 20%

--- resources/views/User/Page/Home/home.blade.php
+++ resources/views/User/Page/Home/home.blade.php
@@ -1,11 +1,79 @@
 @extends('User.master')
+<style>
+  .home-testimonial-bottom {
+    background-color: #f8f8f8;
+    transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s;
+    margin-top: 20px;
+    margin-bottom: 0px;
+    position: relative;
+    height: 130px;
+    top: 190px
+}
+
+.home-testimonial h3 {
+    color: var(--orange);
+    font-size: 14px;
+    font-weight: 500;
+    text-transform: uppercase
+}
+
+.home-testimonial h2 {
+    color: white;
+    font-size: 28px;
+    font-weight: 700
+}
+
+.testimonial-inner {
+    position: relative;
+    top: -174px
+}
+
+.testimonial-pos {
+    position: relative;
+    top: 24px
+}
+
+.testimonial-inner .tour-desc {
+    border-radius: 5px;
+    padding: 40px
+}
+
+.color-grey-3 {
+    font-family: "Montserrat", Sans-serif;
+    font-size: 14px;
+    color: #6c83a2
+}
+
+.testimonial-inner img.tm-people {
+    width: 60px;
+    height: 60px;
+    -webkit-border-radius: 50%;
+    border-radius: 50%;
+    -o-object-fit: cover;
+    object-fit: cover;
+    max-width: none
+}
+
+.link-name {
+    font-family: "Montserrat", Sans-serif;
+    font-size: 14px;
+    color: #6c83a2
+}
+
+.link-position {
+    font-family: "Montserrat", Sans-serif;
+    font-size: 12px;
+    color: #6c83a2
+}
+</style>
+
 
 @section('content')
 
 <div class="container py-3 bg-white" >
     <img src="http://drive.google.com/uc?export=view&id=1YEMvaFTYusWrjThHEGISvpxN4oUZZ5cx" alt="foto" width="559px" height="70px" style="margin-left:auto; margin-right:auto; display:block; padding-top:24px ">
         <div class="row mt-4 justify-content-center">
-          <div class="col-sm-6 d-flex justify-content-center" style="margin-bottom:20px;" >
+          <div class="col d-flex justify-content-center" style="margin-bottom:20px;" >
             <div class="card card-block" style="width: 300px; height:430px; border-radius: 25px; border-solid:1px; border-color:#D77409;">
               <img src="http://drive.google.com/uc?export=view&id=12HjDeFfTn8KY7fKcinR4pxvHugeqG9hg" alt="foto" width="237px" height="235px" style="padding-left:80px; padding-top:48px">
               <div class="card-body">
@@ -16,7 +84,7 @@
             </div>
           </div> 
     
-          <div class="col-sm-6 d-flex justify-content-center" style="margin-bottom:20px;" >
+          <div class="col d-flex justify-content-center" style="margin-bottom:20px;" >
             <div class="card card-block" style="width: 300px; height:430px; border-radius: 25px; border-solid:1px; border-color:#D77409;">
               <img src="http://drive.google.com/uc?export=view&id=1ccVV5FSxeuMvm9WxooFvxQqrOs1CTAGm" alt="foto" width="237px" height="235px" style="padding-left:80px; padding-top:48px">
               <div class="card-body">
@@ -41,6 +109,47 @@
             <p>LOAK.IN merupakan sebuah aplikasi mobile untuk memanggil tukang loak untuk menjual barang bekas. Selain itu, aplikasi LOAK.IN ini juga menyediakan suatu fitur untuk membeli barang bekas yang sudah di daur ulang. Aplikasi ini diciptakan dengan melihat keadaan masyarakat yang sulit untuk menemukan tukang loak dan menyalurkan barang bekas agar dapat didaur ulang.</p>
           </div>
         </div>
+    </div>
+
+    <div class="container py-3 bg-white mt-3" >
+      <h3 style="font-family: 'Nunito'; font-style: normal; font-weight: 800; color:#D77409; text-align:center;">WHAT THEY SAID ABOUT US?</h3>
+
+      <div class="row mt-5 pt-3 justify-content-center">
+
+        <div class="col d-flex justify-content-center" style="margin-bottom:20px;" >
+          <div class="card card-block" style="width: 300px; height:430px; border-radius: 25px; border-solid:3px; border-color:#D77409;">
+            <img src="http://drive.google.com/uc?export=view&id=1GyLd_49s_kqwl48me23PSMS0EmaKv6Df" alt="foto" width="230px" height="150px" style="padding-left:80px; margin-top:20px">
+            <div class="card-body">
+              <h5 class="card-title" style="font-family: Nunito; font-weight:bold; text-align: center; color:#F58814">ORDER LOAK.IN</h5>
+              <h6 class="card-title" style="font-family: Nunito; font-weight:light; text-align: center; font-size:12px; color:#F58814">ORDER LOAK.IN</h6>
+              <p class="card-text" style="font-size:16px; font-family: Nunito; font-weight: 400; text-align: center; padding-top:20px;">Order Loak.in merupakan fitur utama dari Loak.in. Fitur ini berguna untuk memanggil tukang loak terdekat. Selain itu, fitur ini bisa digunakan kapan saja dan dimana saja</p>
+             </div> 
+          </div>
+        </div> 
+
+        <div class="col d-flex justify-content-center" style="margin-bottom:20px;" >
+          <div class="card card-block" style="width: 300px; height:430px; border-radius: 25px; border-solid:3px; border-color:#D77409;">
+            <img src="http://drive.google.com/uc?export=view&id=1GyLd_49s_kqwl48me23PSMS0EmaKv6Df" alt="foto" width="230px" height="150px" style="padding-left:80px; margin-top:20px">
+            <div class="card-body">
+              <h5 class="card-title" style="font-family: Nunito; font-weight:bold; text-align: center; color:#F58814">ORDER LOAK.IN</h5>
+              <h6 class="card-title" style="font-family: Nunito; font-weight:light; text-align: center; font-size:12px; color:#F58814">ORDER LOAK.IN</h6>
+              <p class="card-text" style="font-size:16px; font-family: Nunito; font-weight: 400; text-align: center; padding-top:20px;">Order Loak.in merupakan fitur utama dari Loak.in. Fitur ini berguna untuk memanggil tukang loak terdekat. Selain itu, fitur ini bisa digunakan kapan saja dan dimana saja</p>
+             </div> 
+          </div>
+        </div> 
+
+        <div class="col d-flex justify-content-center" style="margin-bottom:20px;" >
+          <div class="card card-block" style="width: 300px; height:430px; border-radius: 25px; border-solid:3px; border-color:#D77409;">
+            <img src="http://drive.google.com/uc?export=view&id=1GyLd_49s_kqwl48me23PSMS0EmaKv6Df" alt="foto" width="230px" height="150px" style="padding-left:80px; margin-top:20px">
+            <div class="card-body">
+              <h5 class="card-title" style="font-family: Nunito; font-weight:bold; text-align: center; color:#F58814">ORDER LOAK.IN</h5>
+              <h6 class="card-title" style="font-family: Nunito; font-weight:light; text-align: center; font-size:12px; color:#F58814">ORDER LOAK.IN</h6>
+              <p class="card-text" style="font-size:16px; font-family: Nunito; font-weight: 400; text-align: center; padding-top:20px;">Order Loak.in merupakan fitur utama dari Loak.in. Fitur ini berguna untuk memanggil tukang loak terdekat. Selain itu, fitur ini bisa digunakan kapan saja dan dimana saja</p>
+             </div> 
+          </div>
+        </div> 
+
+      </div>
     </div>
 
 @endsection
